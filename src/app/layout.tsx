026c885308f0cc7e06f3b90494cf95a725e534a9
AdamArabov <arabovadam@yahoo.com>
2023-06-25 11:00:50 -0400

    h

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
       <body className={inter.className}>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center">
+  <a href="https://flowbite.com/" className="flex items-center">
       
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <span className=" text-2xl font-semibold whitespace-nowrap dark:text-white">Raid Assistant</span>
   </a>
   <div className="flex md:order-2">
     <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
