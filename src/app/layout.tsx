2a2cb68f14317f7bbe44c9c30903160db1235080
AdamArabov <arabovadam@yahoo.com>
2023-07-25 19:06:45 -0400

    added nightfalls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,9 @@ export default function RootLayout({
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Exotic Quests</a>
         </li>
+        <li>
+          <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Nightfall</a>
+        </li>
       </ul>
     </div>
   </div>
